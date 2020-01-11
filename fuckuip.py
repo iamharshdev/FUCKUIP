0397c483616f1a625a0c772cf6aff540573ece82
theuitown <nitingoswami473@gmail.com>
2020-01-11 22:01:30 +0530

    added auto installation

--- fuckuip.py
+++ fuckuip.py
@@ -1,7 +1,22 @@
-import ipinfo
+def install_and_import(package):
+    import importlib
+    try:
+        importlib.import_module(package)
+    except ImportError:
+        import pip
+        pip.main(['install', package])
+    finally:
+        globals()[package] = importlib.import_module(package)
+        
+install_and_import('ipinfo')
 import pprint
 access_token = '4c80130a1318d7'
 handler = ipinfo.getHandler(access_token)
 ip_address = input("Enter any IP Address")
 details = handler.getDetails(ip_address)
 pprint.pprint(details.all)
+print("\n")
+print("Thanks for using FUCKUIP :)")
+print("Coded by")
+print("HARSH VARDHAN GOSWAMI")
+print("\n")
--- fuckuip.sh
+++ fuckuip.sh
@@ -1,30 +1,35 @@
-
-echo "$(tput setaf 1) ______  $(tput setaf 2)_    _ $(tput setaf 2)  _____ $(tput setaf 1) _  __       _    _        $(tput setaf 2)_____  _____  ";
-echo "$(tput setaf 1)|  ____|$(tput setaf 2)| |  | |$(tput setaf 2) / ____|$(tput setaf 1)| |/ /      | |  | |      $(tput setaf 2)|_   _||  __ \ ";
-echo "$(tput setaf 1)| |__   $(tput setaf 2)| |  | |$(tput setaf 2)| |     $(tput setaf 1)| ' /______ | |  | | ______ $(tput setaf 2)| |  | |__) |";
-echo "$(tput setaf 1)|  __|  $(tput setaf 2)| |  | |$(tput setaf 2)| |     $(tput setaf 1)|  <|______|| |  | ||______$(tput setaf 2)|| |  |  ___/";
-echo "$(tput setaf 1)| |     $(tput setaf 2)| |__| |$(tput setaf 2)| |____ $(tput setaf 1)| . \       | |__| |       $(tput setaf 2)_| |_ | |     ";
-echo "$(tput setaf 1)|_|      $(tput setaf 2)\____/ $(tput setaf 2) \_____|$(tput setaf 1)|_|\_\       \____/       $(tput setaf 2)|_____||_|     ";
+if [ $(dpkg-query -W -f='${Status}' figlet 2>/dev/null | grep -c "ok installed") -eq 0 ];
+then
+  apt-get install --force-yes --yes figlet;
+fi
+printf "$(tput setaf 4)"
+figlet "FUCK-U-IP"
+printf "$(tput setaf 3)"
 printf "\n"
+printf "  coded by  "
+printf "$(tput setaf 2)"
 printf "\n"
-
-printf " \e[1;77m                               coded by\e[0m \n"
-
+figlet "IAMHARSH.DEV"
+printf "$(tput setaf 3)"
+if [ $(dpkg-query -W -f='${Status}' curl 2>/dev/null | grep -c "ok installed") -eq 0 ];
+then
+  apt-get install curl;
+fi
 printf "\n"
-
-echo "                _                                   _                        _            _              ";
-echo "$(tput setaf 1)(_)        $(tput setaf 2)          | |                      | |          | |             ";
-echo "$(tput setaf 1)  _   __ _$(tput setaf 2) _ __ ___  | |__    __ _  _ __  ___ | |__      __| |  ___ __   __";
-echo "$(tput setaf 1)| | / _\` |$(tput setaf 2)| '_ \` _ \ | '_ \  / _\` || '__|/ __|| '_ \    / _\` | / _ \\ \ / /";
-echo "$(tput setaf 1)| || (_| |$(tput setaf 2)| | | | | || | | || (_| || |   \__ \| | | | _| (_| ||  __/ \ V / ";
-echo "$(tput setaf 1)|_| \__,_|$(tput setaf 2)|_| |_| |_||_| |_| \__,_||_|   |___/|_| |_|(_)\__,_| \___|  \_/  ";
+printf "$(tput setaf 2)"
+printf "Your IP"
 printf "\n"
-printf "\n"
-
-printf " \e[1;77mYour IP\e[0m \n"
 curl ifconfig.me
 printf "\n"
-printf " \e[1;77mHere you go\e[0m \n"
 printf "\n"
-pip3 install ipinfo
-python3 fuckuip.py
+printf "$(tput setaf 1)"
+if [ $(dpkg-query -W -f='${Status}' python 2>/dev/null | grep -c "ok installed") -eq 0 ];
+then
+  apt-get install python;
+fi
+printf "\n"
+printf "$(tput setaf 4)"
+printf "Here you go"
+printf "\n"
+printf "\n"
+python fuckuip.py
